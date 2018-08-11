60b1fa000a337fc943cfcf10b5c7d8779e4054a5
BuildTools <unconfigured@null.spigotmc.org>
2018-08-11 12:26:48 +0700

    yas

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ function handleEvent(event) {
     }
   if(kw[1]){
     var kwca = kw[1].split(" ");
-    if(kwca[1]){
+    if(kwca[0]){
     const berdel = {type: 'text', text: "Tolong taruh pertanyaan"}
     return client.replyMessage(event.replyToken, berdel)
     }
